fb4798aa15badd0c64de84f59e4f1154f0479e76
alifrontend499 <aliofficial499@gmail.com>
2018-10-05 09:42:48 +0530

    First commit

---
+++ application/controllers/Aboutus.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Aboutus extends CI_Controller {
+
+	public function index()
+	{
+		echo "<style> body { font-family: calibri; } </style>";
+		echo "<h1>About us</h1>";
+	}
+}
