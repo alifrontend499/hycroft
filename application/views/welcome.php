08569855241e082ae27d4778514ca8320185dfd5
alifrontend499 <aliofficial499@gmail.com>
2018-10-05 10:36:13 +0530

    changes

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,19 +1,34 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Hycroft</title>
-</head>
+<!-- SITETOP start -->
+<?php $this->load->view('layout/site-top'); ?>
+<!-- SITETOP end -->
 <body>
+	<header></header>
 
-	<table>
-		
-	</table>
+	<div class="table-responsive">
+		<table class="table table-striped table-bordered">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Name</th>
+					<th>Description</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>1</td>
+					<td>Ali</td>
+					<td>He is a front end developer. and currently working in MYL (Making You Live). he has 2+ years of experience. he has good knowledge of HTML, CSS, JS, PHP, SQL and some other web related techs. his previous company was Multichannelcreative. its is an UK base company.</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 	
+
+	<!-- SITEbottom start -->
+	<?php $this->load->view('layout/site-bottom'); ?>
+	<!-- SITEbottom end -->
 </body>
 </html>
